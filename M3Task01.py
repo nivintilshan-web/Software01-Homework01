b8014c87e8318be7ac0b07cbec7dec0fded99d2a
nivntilshan-web <nivintilshan@gmail.com>
2025-09-08 01:34:01 +0530

    Revert "Add files via upload"
    This reverts commit 4cc734a330c572d59357e4f3de68447dbb82ce0e.

--- M3Task01.py
+++
@@ -1,5 +0,0 @@
-length = float(input("Enter the length of the zander (in cm): "))
-if length < 42:
-    print("The zander is", 42 - length, "cm below the size limit. Please release it back into the lake.")
-else:
-    print("The zander meets the size limit. You may keep it.")
